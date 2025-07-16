d59596a2dcc49f1b6d1f8b9d7adfb63193498d29
Jasec <jasec_7@hotmail.com>
2025-07-16 01:23:46 -0700

    complete python p3 loops

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,26 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+        print('Happy New Year!')
+
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    int_list = [list ** 2 for list in int_list]
+    return int_list
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for x in range(1, 101):
+        if (x % 3 == 0 and x % 5 == 0):
+            print("FizzBuzz")
+        elif x % 3 == 0:
+            print("Fizz")
+        elif x % 5 == 0:
+            print("Buzz")
+        else:
+            print(x)
